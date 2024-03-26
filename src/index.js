fa432293384db0c9d953b1fadd1b04e2a6402458
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-03-26 18:55:31 -0300

    Analytics

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import "./assets/styles/global.css";
 import { Home } from './page/Home';
 import { Footer } from './components/Footer';
 import { Menu } from './components/Menu';
+import { Analytics } from "@vercel/analytics/react"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +15,7 @@ root.render(
     <Menu/>
     <Home/>
     <Footer />
+    <Analytics />
   </React.StrictMode>
 );
 
