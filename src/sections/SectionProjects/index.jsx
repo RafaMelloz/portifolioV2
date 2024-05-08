85f9ba134a0e31f6b7d664b73a0ef11cc017fae4
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2024-05-08 17:53:25 -0300

    catalogo digital

--- src/sections/SectionProjects/index.jsx
+++ src/sections/SectionProjects/index.jsx
@@ -2,12 +2,13 @@ import { CardProjects } from "../../components/Card-Projects";
 
 import academyKids from "../../assets/bg-projetos/BG-academyKids.png";
 import appTask from "../../assets/bg-projetos/BG-appTask.png";
-import ensolarado from "../../assets/bg-projetos/BG-ensolarado.png";
 import game from "../../assets/bg-projetos/BG-game.png";
 import orangix from "../../assets/bg-projetos/BG-orangix.png";
 import pikachu from "../../assets/bg-projetos/BG-pikachu.png";
 import vtex from "../../assets/bg-projetos/BG-vtex.png";
 import git from "../../assets/bg-projetos/BG-git.png";
+import catalogo from "../../assets/bg-projetos/BG-catalogo.png";
+
 
 import { FaGithub } from "react-icons/fa";
 
@@ -34,10 +35,11 @@ export const SectionProjects = () =>{
                 <div className="grid__proj">
                     <CardProjects name={'VTEX! (Teste frontEnd)'} img={vtex} link={'https://teste-vaga-front-end-jr.vercel.app'} />
                     <CardProjects name={'Game Calendar'} img={game} link={'https://game-calendarf.vercel.app'} />
-                    <CardProjects name={'Orangix (fecht API)'} img={orangix} link={'https://orangix.vercel.app'} />
-                    <CardProjects name={'appTask'} img={appTask} link={'https://task-appr.vercel.app'} />
                     <CardProjects name={'Pokedex'} img={pikachu} link={'https://pokedex-rafa.vercel.app'} />
+                    <CardProjects name={'appTask'} img={appTask} link={'https://task-appr.vercel.app'} />
+                    <CardProjects name={'Catalogo Digital'} img={catalogo} link={'https://catalogo-digital.vercel.app'} />
                     <CardProjects name={'GitHub Search'} img={git} link={'https://rafa-git-search.vercel.app'} />
+                    <CardProjects name={'Orangix (fecht API)'} img={orangix} link={'https://orangix.vercel.app'} />
                     <CardProjects name={'Kids Academy! (TCC)'} img={academyKids} link={'https://github.com/RafaMelloz/kidsAcademy'} />
                 </div>
             </div>
